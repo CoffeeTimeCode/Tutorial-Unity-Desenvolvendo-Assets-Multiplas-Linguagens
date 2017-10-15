b8e714240521493a773671e3e38af916941ce225
jsnunes <jesiel.nunes@hotmail.com>
2017-10-15 19:59:21 -0200

    #1

---
+++ Assets/Scripts/ControladorArquivos.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ControladorArquivos {
+	private string _caminho;
+
+	public ControladorArquivos(){
+		this._caminho = Application.dataPath;
+	}
+
+	public bool validarDiretorio(string nome){
+		return Directory.Exists (this._caminho+"/"+nome);
+	}
+
+	public bool validarArquivo(string nome){
+		return File.Exists (this._caminho+"/"+nome);
+	}
+
+	public void criarDiretorio(string nome){
+		if (this.validarDiretorio (nome)) {
+			Debug.Log("Esse diretorio ja existe.");
+		} else {
+			Directory.CreateDirectory(this._caminho+"/"+nome);
+		}
+	}
+
+	public void criarArquivo(string nome, string conteudo, bool sobrescrever=false){
+		if (this.validarArquivo (nome)&&!sobrescrever) {
+			Debug.Log("Esse arquivo ja existe");
+		} else {
+			var arquivo = File.CreateText(this._caminho+"/"+nome);
+			arquivo.WriteLine(conteudo);
+			arquivo.Close();
+		}
+	}
+
+	public string[] listarArquivos(string nome, string extesao){
+		if(this.validarDiretorio (nome)){
+			string[] arquivos = Directory.GetFiles(this._caminho+"/"+nome,extesao);
+			for (int i = 0; i < arquivos.Length; i++) {
+				arquivos [i] = arquivos [i].Replace (this._caminho+"/"+nome+"\\","");
+			}
+			return arquivos;
+		}else{
+			Debug.Log("Esse arquivo não existe");
+		}
+		return null;
+	}
+
+	public string lerArquivo(string nome){
+		if (this.validarArquivo (nome)) {
+			var arquivo = File.OpenText(this._caminho+"/"+nome);
+			string temp = arquivo.ReadLine().ToString();
+			arquivo.Close();
+			return temp;
+		} else {
+			Debug.Log("Esse arquivo nao existe");
+			return "";
+		}
+	}
+
+	public T lerJson<T>(string nome){
+		string json = this.lerArquivo (nome);
+		if (json != "") {
+			return JsonUtility.FromJson<T> (json);
+		} else {
+			return default(T);
+		}
+	}
+}
---
+++ Assets/Scripts/ControladorLinguagens.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorLinguagens : MonoBehaviour {
+	public string[] _linguagens;
+	public Linguagem _linguagemSelecionada;
+
+	private ControladorArquivos _ctlArquivos;
+	// Use this for initialization
+	void Start () {
+		this._ctlArquivos = new ControladorArquivos ();
+		this.lerLinguagem ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void lerLinguagem(){
+		if(this._ctlArquivos.validarDiretorio("linguagens")){
+			this._linguagens = this._ctlArquivos.listarArquivos ("linguagens","*.json");
+			this.selecionarLinguagem ("en");
+		}else{
+			this._ctlArquivos.criarDiretorio ("linguagens");
+			Linguagem padrao = new Linguagem ();
+			padrao.nome_linguagem = "Português";
+
+			this._linguagemSelecionada = padrao;
+			this._ctlArquivos.criarArquivo ("linguagens/pt-br.json",JsonUtility.ToJson(padrao));
+			this._linguagens = this._ctlArquivos.listarArquivos ("linguagens","*.json");
+		}
+	}
+
+	public void selecionarLinguagem(string nome){
+		_linguagemSelecionada = this._ctlArquivos.lerJson<Linguagem> ("linguagens/"+nome+".json");
+	}
+}
